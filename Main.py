8d39c13c544f448dcad629cba0451e98fccc7ad0
jsevamo <j_sevamo@hotmail.com>
2020-05-13 12:46:39 -0500

    Added NumPy arrays and vertices

--- Main.py
+++ Main.py
@@ -3,32 +3,43 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+import numpy as np
+
+vertices = np.array(
+    [
+        [0.5, 0, 0],
+        [0.1, 0, 0]
+    ]
+)
+
+print(vertices)
 
 
 def DrawPoints():
     glBegin(GL_POINTS)
-    glVertex3f(0,0,0)
+    for vertex in vertices:
+        glVertex3f(vertex[0], vertex[1], vertex[2])
     glEnd()
+
 
 def main():
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -5)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        # glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
 
         # Draw functions here
 
         DrawPoints()
+
+
 
         pygame.display.flip()
         pygame.time.wait(10)
